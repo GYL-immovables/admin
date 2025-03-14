b04051ccf002c9ff06f6bae01d94b89fa826fab4
koona0 <59355786+koona-0@users.noreply.github.com>
2025-03-14 09:00:57 +0900

    Add files via upload

--- src/main/java/admin/findok_pw.java
+++ src/main/java/admin/findok_pw.java
@@ -9,41 +9,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class findok_id extends HttpServlet {
+public class findok_pw extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	PrintWriter pw = null;
 	m_selectid si = new m_selectid();
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		
 		try {
 			this.pw = response.getWriter();
+			String aid = request.getParameter("aid");
 			String aname = request.getParameter("aname");
 			String atel = request.getParameter("atel");
 			String aemail = request.getParameter("aemail");
-			String aid = this.si.idok(aname,atel,aemail);
 			
-			if (aid == null) {
-			    this.pw.write("<script>" 
-			            + "alert('해당되는 아이디가 없습니다.');" 
+			String selid = this.si.idok(aname,atel,aemail);
+			
+			if(aid.equals(selid)){
+				request.setAttribute("aid", aid);
+				RequestDispatcher rd = request.getRequestDispatcher("./update_pw.jsp");
+				rd.forward(request, response);
+			}else {
+				this.pw.write("<script>" 
+			            + "alert('정보를 다시 확인해주세요');" 
 			            + "history.go(-1);" 
 			            + "</script>");
-			}else {
-				request.setAttribute("aid", aid);
-
-				RequestDispatcher rd = request.getRequestDispatcher("./findok_id.jsp");
-				rd.forward(request, response);
 			}
 		}catch (Exception e) {
 			this.pw.write("<script>" 
-					+ "alert('findok_id error');" 
+					+ "alert('findok_pw error');" 
 					+ "history.go(-1);" 
 					+ "</script>");
 		}finally {
 			this.pw.close();
 		}
 	}
-
 }
