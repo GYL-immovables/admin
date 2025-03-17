8ad05a7f68b497a10fd3a9041369bf6e733f4f6d
koona-0 <nayoung9909@naver.com>
2025-03-17 18:07:24 +0900

    koo

--- src/main/java/admin/m_md5.java
+++ src/main/java/admin/m_md5.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package admin;
 
 import java.security.MessageDigest;
@@ -21,27 +21,4 @@ public class m_md5 {
 		return md5_data;
 	}
 }
-=======
-package admin;
 
-import java.security.MessageDigest;
-
-public class m_md5 {
-	public String md5_code(String pw) throws Exception{
-		StringBuilder sb = new StringBuilder();
-		String md5_data = "";	//사용자가 입력한 값을 md5로 암호화하는 변수
-		
-		MessageDigest md = MessageDigest.getInstance("md5");
-		md.update(pw.getBytes());
-		byte md5byte[] = md.digest();
-		
-		//암호화 알고리즘 
-		for(byte alg : md5byte) {
-			sb.append(String.format("%01x", alg));
-		}
-		md5_data = String.valueOf(sb);
-
-		return md5_data;
-	}
-}
->>>>>>> branch 'main' of https://github.com/GYL-immovables/admin.git
